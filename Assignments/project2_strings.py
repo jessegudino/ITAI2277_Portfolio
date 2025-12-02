3f9b5c63dcf4048253b7e4f6e2dc448dc801d143
jessegudino <jessegudino@outlook.com>
2025-12-01 20:47:28 -0600

    Add project2_strings.py for string manipulation

---
+++ Assignments/project2_strings.py
@@ -0,0 +1,12 @@
+# Project 2: Working With Strings - Jesus Gudino
+
+first_name = "Jesus"
+last_name = "Gudino"
+
+full_name = first_name + " " + last_name
+message = f"Hello, my name is {full_name} and I'm learning Python!"
+
+print(message)
+
+print("Uppercase:", full_name.upper())
+print("Lowercase:", full_name.lower())
